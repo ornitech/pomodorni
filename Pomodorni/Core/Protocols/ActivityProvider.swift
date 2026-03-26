8f7ce171df0608e25e4f0e5b04d8c9d584974410
gv94 <victor@ornitech.se>
2026-03-26 10:46:28 +0100

    feat: add SystemActivityProvider with NSEvent global monitoring

--- Pomodorni/Core/Protocols/ActivityProvider.swift
+++ Pomodorni/Core/Protocols/ActivityProvider.swift
@@ -1,6 +1,25 @@
-import Foundation
+import AppKit
 
 protocol ActivityProvider: AnyObject {
     func startMonitoring(onActivity: @escaping () -> Void)
     func stopMonitoring()
+}
+
+/// Production activity provider using global NSEvent monitoring.
+final class SystemActivityProvider: ActivityProvider {
+    private var monitor: Any?
+
+    func startMonitoring(onActivity: @escaping () -> Void) {
+        stopMonitoring()
+        monitor = NSEvent.addGlobalMonitorForEvents(matching: [.mouseMoved, .keyDown]) { _ in
+            onActivity()
+        }
+    }
+
+    func stopMonitoring() {
+        if let monitor {
+            NSEvent.removeMonitor(monitor)
+        }
+        monitor = nil
+    }
 }
